9a3bc9d81845243454658fde1d18375a775b5492
aureliamarzipan <aureliakornheiser@gmail.com>
2026-01-15 13:12:25 -0500

    Add delay

--- simulate.py
+++ simulate.py
@@ -1,8 +1,13 @@
 import pybullet as p
-
-
+import time
 
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0) # hides sidebars
+
+for i in range(1000):
+    p.stepSimulation()
+    print(i)
+    time.sleep(.01)
 
 p.disconnect()
 
