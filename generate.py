08990830760c4d878a8437af4be98d8777e88232
aureliamarzipan <aureliakornheiser@gmail.com>
2026-01-20 11:36:45 -0500

    add a box

---
+++ generate.py
@@ -0,0 +1,7 @@
+import pyrosim.pyrosim as pyrosim
+
+pyrosim.Start_SDF("box.sdf") # create a file to store world
+
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1]) # make a cube
+
+pyrosim.End() # close sdf file
