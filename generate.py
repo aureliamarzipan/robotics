c901a104ec310731d2af3b02fd7baa096c00eb61
aureliamarzipan <aureliakornheiser@gmail.com>
2026-01-20 13:51:06 -0500

    make stack of 10 boxes

--- generate.py
+++ generate.py
@@ -1,7 +1,13 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf") # create a file to store world
+pyrosim.Start_SDF("boxes.sdf") # create a file to store world
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1]) # make a cube
+size = 1
+pos = 0
+for i in range(10):
+    pos = pos + size/2 # add to middle of new box
+    pyrosim.Send_Cube(name="Box" + str(i), pos=[0,0, pos] , size=[size,size,size]) # make a cube
+    pos = pos + size/2 # add to top of new box
+    size = size * 0.9 # update box size
 
 pyrosim.End() # close sdf file
--- simulate.py
+++ simulate.py
@@ -1,12 +1,17 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) # gets predefined objects e.g. plane.urdf
 p.configureDebugVisualizer(p.COV_ENABLE_GUI,0) # hides sidebars
 
-p.loadSDF("box.sdf")
+p.setGravity(0,0,-9.8)
 
-for i in range(1000):
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
+
+for i in range(100000):
     p.stepSimulation()
     print(i)
     time.sleep(.01)
