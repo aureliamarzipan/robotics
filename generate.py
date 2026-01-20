9012e9219c9724bf937fcbbb23136d9d6107c17b
aureliamarzipan <aureliakornheiser@gmail.com>
2026-01-20 13:53:50 -0500

    make 25 towers

--- generate.py
+++ generate.py
@@ -2,12 +2,14 @@ import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("boxes.sdf") # create a file to store world
 
-size = 1
-pos = 0
-for i in range(10):
-    pos = pos + size/2 # add to middle of new box
-    pyrosim.Send_Cube(name="Box" + str(i), pos=[0,0, pos] , size=[size,size,size]) # make a cube
-    pos = pos + size/2 # add to top of new box
-    size = size * 0.9 # update box size
+for x in range(5):
+    for y in range(5):
+        size = 1
+        z = 0
+        for i in range(10):
+            z = z + size/2 # add to middle of new box
+            pyrosim.Send_Cube(name="Box" + str(i), pos=[x,y, z] , size=[size,size,size]) # make a cube
+            z = z + size/2 # add to top of new box
+            size = size * 0.9 # update box size
 
 pyrosim.End() # close sdf file
